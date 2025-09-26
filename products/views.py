02dc74f1dbc6f8da1a8f29e3e6a02e87d3bae54e
Sherif Mohamed <sherif425@gmail.com>
2025-09-26 11:57:48 +0300

    docs(api): integrate drf-spectacular for API schema and Swagger UI

--- products/views.py
+++ products/views.py
@@ -3,10 +3,12 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter, SearchFilter
 from .models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_permissions(self):
         if self.action in ['list', 'retrieve']:
@@ -20,6 +22,7 @@ class ProductViewSet(viewsets.ModelViewSet):
     filterset_fields = ['category', 'is_active']
     search_fields = ['name', 'description']
     ordering_fields = ['price', 'created_at', 'name']
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_permissions(self):
         if self.action in ['list', 'retrieve']:
