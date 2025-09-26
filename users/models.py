61a415d6a03b9bb285f6f1f500e1b1d9a0d471dc
Sherif Mohamed <sherif425@gmail.com>
2025-09-26 16:17:42 +0300

    feat(users): implement JWT-based user authentication

--- users/models.py
+++ users/models.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
-    # Extend later if needed
-    pass
+    # username = models.CharField(max_length=50)
+    address=models.CharField(max_length=100, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
